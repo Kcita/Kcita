5587c432bb7dd7d11f63b4fbf381180bee0f3deb
Paolo <paolo@Paolos-MacBook-Pro-2.local>
2019-06-20 11:48:49 -0500

    Update reserva.py

--- src/reserva.py
+++ src/reserva.py
@@ -15,14 +15,15 @@ class Reserva:
         - fecha_de_inicio_de_reserva: datetime
         - fecha_de_fin_de_reserva: datetime
         - precio_total: float
+        - pago_atrasado: boolean
         - anulado: boolean
         - pagos: array
         ----------------------------------------------------------
         Métodos:
-        - Casa
-        - Habitacion
         - numero_de_noches
         - monto_cobrado
+        - adelanto_esta_atrasado
+        - pagado
         """
 
     def __init__(self, cliente, paquete, fecha_de_creacion, fecha_de_inicio_de_reserva):
@@ -32,10 +33,12 @@ class Reserva:
         self.codigo_reserva = codigo_reserva
         self.cliente = cliente
         self.paquete = paquete
+        self.fecha_de_creacion = fecha_de_creacion
         self.fecha_de_inicio_de_reserva = fecha_de_inicio_de_reserva
         self.fecha_de_fin_de_reserva = fecha_de_inicio_de_reserva + datetime.timedelta(days = paquete.numero_de_dias - 1)
         self.numero_de_noches = (self.fecha_de_fin_de_reserva - self.fecha_de_inicio_de_reserva).days
         self.precio_total = paquete.numero_de_dias * paquete.precio_por_dia
+        self.pago_atrasado = False
         self.dias = []
         for i in range(0, self.numero_de_noches + 1):
             self.dias.append(self.fecha_de_inicio_de_reserva + datetime.timedelta(days = i))
@@ -56,3 +59,19 @@ class Reserva:
         for pago in self.pagos:
             monto_cobrado += pago.monto
         return monto_cobrado
+
+    def anular(self):
+        """Cambia el atributo anulado a True"""
+        self.anulado = True
+
+    def adelanto_esta_atrasado(self):
+        """Devuelve True si el pago está atrasado, devuelve False caso contrario"""
+        hoy_dia = datetime.date.today()
+        if (hoy_dia - self.fecha_de_creacion).days >= 3 and self.monto_cobrado <= 0.2 * self.precio_total:
+            return True
+        else:
+            return False
+
+    def pagado(self):
+        """Devuelve True si la reserva ha sido pagada, devuelve False caso contrario"""
+        if self.monto_cobrado() == self.precio_total:
